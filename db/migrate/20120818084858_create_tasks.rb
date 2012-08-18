4127769a1cd4f834eed7e52a2804dada4809520a
Juliusz Gonera <juliusz.gonera@gmail.com>
2012-08-18 11:19:12 +0200

    Add Task model

---
+++ db/migrate/20120818084858_create_tasks.rb
@@ -0,0 +1,11 @@
+class CreateTasks < ActiveRecord::Migration
+  def change
+    create_table :tasks do |t|
+      t.string :title, :description
+      t.date :due
+      t.integer :priority
+
+      t.timestamps
+    end
+  end
+end
