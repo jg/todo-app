cec4bca077f978b5304441eb3ed90ab814fc5b0a
Juliusz Gonera <juliusz.gonera@gmail.com>
2012-10-05 17:05:56 +0200

    Add Users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Task.find_or_create_by_title('Wash dishes', {:due => Date.today+1, :priority => 12}, :without_protection => true)
-Task.find_or_create_by_title('Walk dog', {:description => 'Feed him, too', :due => Date.today+2, :priority => 1}, :without_protection => true)
-Task.find_or_create_by_title('Pay for language courses', {:description => 'uhh', :due => Date.today+2, :priority => 1}, :without_protection => true)
+user = User.find_or_create_by_email('juliusz.gonera@gmail.com', {:encrypted_password => '51abb9636078defbf888d8457a7c76f85c8f114c'}, :without_protection => true)
+Task.find_or_create_by_title('Wash dishes', {:due => Date.today+1, :priority => 12, :user => user}, :without_protection => true)
+Task.find_or_create_by_title('Walk dog', {:description => 'Feed him, too', :due => Date.today+2, :priority => 1, :user => user}, :without_protection => true)
+Task.find_or_create_by_title('Pay for language courses', {:description => 'uhh', :due => Date.today+2, :priority => 1, :user => user}, :without_protection => true)
