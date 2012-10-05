d2dced8e2d1a7b8e011a14a566d05af6ec1779a5
Juliusz Gonera <juliusz.gonera@gmail.com>
2012-10-05 17:06:37 +0200

    Add Http Basic Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,25 @@
+require 'digest/sha1'
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :authenticate
+
+  protected
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |given_username, given_password|
+      if user = User.find_by_email(given_username)
+        password = user.encrypted_password
+        if Digest::SHA1.hexdigest(given_password) == password
+          @current_user = user
+          true
+        else
+          false
+        end
+      end
+    end
+  end
+
+  def current_user
+    @current_user
+  end
 end
